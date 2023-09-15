e6f7e24be513ab6be2ba5030d0dbd1ab3b78042a
Bilal-EZ-ZAIM <bilalzaim499@gmail.com>
2023-09-15 09:57:09 +0100

    challenge8

---
+++ challenge8.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+// Challenge 8 :  Affichage de la valeur octale et hexadécimale équivalente
+// prof Aymane
+// prof Said
+// étudiant Bilal EZZAIM
+// function welcom to project
+void premierFonction()
+{
+    printf("                 =================================================================================\n");
+    printf("                 =================================================================================\n");
+    printf("                 ==== Challenge 1 : Affichage de la valeur octale et hexadécimale équivalente ====\n");
+    printf("                 =================================================================================\n");
+    printf("                 =================================================================================\n");
+}
+
+int main()
+{
+    premierFonction();
+    int x ;
+    printf("ecrivez le némiro : ");
+    scanf("%d",&x);
+    printf("Octale = %o\n",x);
+    printf("hexadécimale = %x\n",x);
+    return 0;
+}
